b78732a8ce9142d2c8e32146c0cf8f837f66f89d
Parkduksung <duksung1234@naver.com>
2021-03-01 23:58:42 +0900

    [refactor-splash]splash 하면서 데이터 확인 - 세팅

--- app/src/androidTest/java/com/work/toy_zigzag/view/splash/SplashActivityTest.kt
+++ app/src/androidTest/java/com/work/toy_zigzag/view/splash/SplashActivityTest.kt
@@ -1,11 +1,18 @@
 package com.work.toy_zigzag.view.splash
 
+import android.content.Context
+import androidx.room.Room
 import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.work.toy_zigzag.R
+import com.work.toy_zigzag.network.room.dao.ShoppingDao
+import com.work.toy_zigzag.network.room.database.ShoppingDatabase
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -13,6 +20,29 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class SplashActivityTest {
 
+    companion object {
+        private const val FILE_NAME = "shop_list.json"
+    }
+
+    private lateinit var shoppingDatabase: ShoppingDatabase
+
+    private lateinit var shoppingDao: ShoppingDao
+
+    @Before
+    fun init() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        shoppingDatabase = Room.inMemoryDatabaseBuilder(
+            context,
+            ShoppingDatabase::class.java
+        ).build()
+
+        shoppingDao = shoppingDatabase.shoppingListDao()
+    }
+
+    @After
+    fun tearDown() {
+        shoppingDatabase.close()
+    }
 
     @Test
     fun should_show_title_when_launch_app() {
