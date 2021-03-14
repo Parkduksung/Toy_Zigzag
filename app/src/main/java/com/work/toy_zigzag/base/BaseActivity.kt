fcde559cf6aa80cf42813ddcb3af0c5e578fa5b8
Parkduksung <duksung1234@naver.com>
2021-03-14 16:06:20 +0900

    [refactor-splash] baseActivity, SplashViewModel 추가.

---
+++ app/src/main/java/com/work/toy_zigzag/base/BaseActivity.kt
@@ -0,0 +1,23 @@
+package com.work.toy_zigzag.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<T : ViewDataBinding>(@LayoutRes private val layoutId: Int) :
+    AppCompatActivity() {
+
+    protected lateinit var binding: T
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        binding.lifecycleOwner = this
+
+        setContentView(binding.root)
+
+    }
+}
---
+++ app/src/main/java/com/work/toy_zigzag/view/splash/SplashViewModel.kt
@@ -0,0 +1,13 @@
+package com.work.toy_zigzag.view.splash
+
+import androidx.lifecycle.ViewModel
+import com.work.toy_zigzag.data.repository.ShoppingRepository
+
+class SplashViewModel(
+    private val shoppingRepository: ShoppingRepository
+) : ViewModel() {
+
+
+
+
+}
