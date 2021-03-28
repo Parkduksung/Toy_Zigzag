9eeeba10189393f8d2942dc460478af228fc08a2
Parkduksung <duksung1234@naver.com>
2021-03-29 00:06:33 +0900

    [feature] baseViewModel 에 코루틴 추가.

--- app/src/main/java/com/work/toy_zigzag/base/BaseViewModel.kt
+++ app/src/main/java/com/work/toy_zigzag/base/BaseViewModel.kt
@@ -4,6 +4,9 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
 
 abstract class BaseViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -11,12 +14,18 @@ abstract class BaseViewModel(application: Application) : AndroidViewModel(applic
     private val _viewStateLiveData = MutableLiveData<ViewState>()
     val viewStateLiveData: LiveData<ViewState> = _viewStateLiveData
 
+    protected val viewModelScope = CoroutineScope(Dispatchers.Main)
 
     protected fun viewStateChanged(viewState: ViewState){
         _viewStateLiveData.value = viewState
         _viewStateLiveData.value = null
     }
 
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelScope.cancel()
+    }
 }
 
 interface ViewState
