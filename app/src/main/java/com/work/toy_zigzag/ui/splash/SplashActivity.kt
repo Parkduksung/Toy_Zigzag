dd6833d98ed6598fe3365462a6d0731bef6cc7a9
Parkduksung <duksung1234@naver.com>
2021-04-04 14:13:45 +0900

    [feature-koin] kotlin inject 로 변경해보기.

--- app/src/main/java/com/work/toy_zigzag/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/work/toy_zigzag/ui/splash/SplashActivity.kt
@@ -7,11 +7,12 @@ import com.work.toy_zigzag.R
 import com.work.toy_zigzag.base.BaseActivity
 import com.work.toy_zigzag.databinding.SplashBinding
 import com.work.toy_zigzag.ui.shopping.main.ShoppingActivity
-import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.android.ext.android.inject
 
 class SplashActivity : BaseActivity<SplashBinding>(R.layout.splash) {
 
-    private val splashViewModel by viewModel<SplashViewModel>()
+    private val splashViewModel by inject<SplashViewModel>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
